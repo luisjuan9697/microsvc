223b2ae93e8e172d45f114d8870e1be8cf0cfa1a
Luis <luis.luisjuan@udgvirtual.udg.mx>
2024-07-01 07:31:31 -0600

    add-solution-first

---
+++ jenkins-server-tf/backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket         = "cicdsre"
+    region         = "us-east-1"
+    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
